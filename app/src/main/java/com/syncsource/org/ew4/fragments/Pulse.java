2e2aa66454a46e6b3a502a5cefaf48eee85519a2
nalioes <nalioesyn@gmail.com>
2016-07-02 15:52:43 +0630

    fix image

--- app/src/main/java/com/syncsource/org/ew4/fragments/Pulse.java
+++ app/src/main/java/com/syncsource/org/ew4/fragments/Pulse.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.syncsource.org.ew4.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by SyncSource on 7/1/2016.
@@ -16,10 +21,21 @@ public class Pulse extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.pulse_fragment,container,false);
+        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.pulse_fragment, container, false);
+        CircleImageView imgAvater = (CircleImageView) view.findViewById(R.id.avater_image);
+        ImageView contentImage = (ImageView) view.findViewById(R.id.content_image);
 
+        Glide.with(getContext())
+                .load(R.drawable.elizabeth)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgAvater);
+        Glide.with(getContext())
+                .load(R.drawable.elizabeth_show)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(contentImage);
         return view;
     }
+
     public static Pulse newInstance() {
         Pulse fragment = new Pulse();
         Bundle bundle = new Bundle();
